e37f251a1889a6b860c0d9ac4d90a7872eec28d2
amaro-netto <amarosilva002@gmail.com>
2025-09-16 16:28:12 -0300

    coreeção de alinhamento de seção e imagem

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -30,7 +30,7 @@ const HeroSection = () => {
       <div className="relative container mx-auto px-4 h-full">
         <div className="flex flex-col lg:flex-row justify-between h-full min-h-screen pt-20">
           {/* Image Side - Left */}
-          <div className="flex-1 flex justify-center lg:justify-start mb-12 lg:mb-0 order-1 lg:order-1 lg:self-end">
+          <div className="flex-1 flex justify-center lg:justify-start mb-12 lg:mb-0 order-1 lg:order-1 lg:self-start">
             <div className="relative h-full w-full max-w-xl min-h-[680px]">
               <div className="absolute inset-0 bg-gradient-primary blur-3xl opacity-20 transform -rotate-6 scale-105"></div>
               <img
