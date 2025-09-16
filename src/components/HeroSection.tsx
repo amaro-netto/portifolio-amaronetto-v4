4d423e6eb8487277324fdfc0b142da7135ec2221
amaro-netto <amarosilva002@gmail.com>
2025-09-16 17:00:41 -0300

    Correção final

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -28,10 +28,10 @@ const HeroSection = () => {
       <div className="absolute bottom-20 left-20 w-96 h-96 bg-accent/10 rounded-full blur-3xl"></div>
       
       <div className="relative mx-auto px-4 h-full">
-        <div className="flex flex-col lg:flex-row justify-between h-full min-h-screen">
+        <div className="flex flex-col lg:flex-row lg:items-end justify-between h-full min-h-[calc(100vh_-_80px)] pt-20">
           {/* Image Side - Left */}
           <div className="flex-1 flex justify-center lg:justify-start mb-12 lg:mb-0 order-1 lg:order-1 lg:self-end">
-            <div className="relative h-full w-full max-w-2xl min-h-[400px] lg:min-h-[816px]">
+            <div className="relative h-full w-full max-w-2xl min-h-[400px] lg:min-h-[70vh]">
               <div className="absolute inset-0 bg-gradient-primary blur-3xl opacity-20 transform -rotate-6 scale-105"></div>
               <img
                 src={amaroPortrait}
@@ -43,7 +43,7 @@ const HeroSection = () => {
           </div>
           
           {/* Content Side - Right */}
-          <div className="flex-1 text-center lg:text-left order-2 lg:order-2 lg:pt-40 container">
+          <div className="flex-1 text-center lg:text-left order-2 lg:order-2 container">
             <div className="space-y-8">
               <div className="space-y-4">
                 <h1 className="font-display text-4xl md:text-5xl lg:text-6xl font-bold text-white leading-tight">
