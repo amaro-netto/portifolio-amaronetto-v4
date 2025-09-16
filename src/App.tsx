2d3dd8fc886e8bc2d49b51e7f2e3563eedb87cb9
amaro-netto <amarosilva002@gmail.com>
2025-09-16 19:56:58 -0300

    adição de mais analises e melhorias no design.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const queryClient = new QueryClient();
 
