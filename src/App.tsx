52b8248d18c17949b8cabb255a9244de22621798
amaro-netto <amarosilva002@gmail.com>
2025-09-17 23:45:48 -0300

    feat: Integra Vercel Analytics e Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const queryClient = new QueryClient();
 
@@ -22,6 +22,9 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      {/* NOVO: Componentes do Vercel Analytics e Speed Insights adicionados */}
+      <Analytics />
+      <SpeedInsights />
     </TooltipProvider>
   </QueryClientProvider>
 );
